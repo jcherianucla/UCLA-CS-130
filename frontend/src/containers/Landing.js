82f9f30e33fd9909b1919b3a12e4053c0522d9c6
Omar Ozgur <oozgur217@gmail.com>
2017-11-06 18:12:24 -0800

    Add hot reloading

--- frontend/src/containers/Landing.js
+++ frontend/src/containers/Landing.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
 import '../styles/Landing.css';
 
 class Landing extends Component {
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader'
 import './styles/index.css';
 import Landing from './containers/Landing';
 // import registerServiceWorker from './utils/registerServiceWorker';
 
-ReactDOM.render(<Landing />, document.getElementById('root'));
+ReactDOM.render(
+  <AppContainer>
+    <Landing />
+  </AppContainer>,
+  document.getElementById('root')
+  );
 // registerServiceWorker();
