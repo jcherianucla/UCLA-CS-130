adbd41da30b01c12e3b242a740c4861198e6f5e5
jcherianucla <jcherian@ucla.edu>
2017-11-09 21:12:26 -0800

    Remove db information constant

--- api/utilities/db.go
+++ api/utilities/db.go
@@ -1,29 +1,10 @@
 package utilities
 
-import (
-	"fmt"
-	"os"
-)
-
 const (
 	DB_USER     = "gradeportal"
+	DB_PORT     = "5234"
 	DB_PASSWORD = "cs130gp"
 	// TODO: Make this dependent on the application environment
+	DB_HOST = "localhost"
 	DB_NAME = "gp_development"
 )
-
-var DB_INFO = func() string {
-
-	dbInfo := os.Getenv("DB_INFO")
-
-	if dbInfo == "" {
-		dbInfo = fmt.Sprintf(
-			"user=%s password=%s dbname=%s sslmode=disable",
-			DB_USER,
-			DB_PASSWORD,
-			DB_NAME,
-		)
-		os.Setenv("DB_INFO", dbInfo)
-	}
-	return dbInfo
-}()
