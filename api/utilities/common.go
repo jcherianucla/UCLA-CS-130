f1025e5f4fbe5951c93353d8e7d6fd7d92ba8c09
jcherianucla <jcherian@ucla.edu>
2017-12-06 22:45:37 -0800

    Add utility to create struct from interface

--- api/utilities/common.go
+++ api/utilities/common.go
@@ -1,6 +1,10 @@
 package utilities
 
-import "reflect"
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"reflect"
+)
 
 // IsUndeclared uses reflection to see if the
 // value of the field is set or not.
@@ -10,10 +14,33 @@ func IsUndeclared(v interface{}) bool {
 	return reflect.DeepEqual(v, reflect.Zero(reflect.TypeOf(v)).Interface())
 }
 
-func FillStruct(data map[string]interface{}, result interface{}) {
-	t := reflect.ValueOf(result).Elem()
-	for k, v := range data {
-		val := t.FieldByName(k)
-		val.Set(reflect.ValueOf(v))
+func SetField(obj interface{}, key string, value interface{}) error {
+	structFieldValue := reflect.ValueOf(obj).Elem().FieldByName(key)
+
+	if !structFieldValue.IsValid() {
+		return fmt.Errorf("No such field: %s in obj", key)
 	}
+
+	if !structFieldValue.CanSet() {
+		return fmt.Errorf("Cannot set %s field value", key)
+	}
+
+	structFieldType := structFieldValue.Type()
+	val := reflect.ValueOf(value)
+	if structFieldType != val.Type() {
+		return errors.New("Provided value type didn't match obj field type")
+	}
+
+	structFieldValue.Set(val)
+	return nil
+}
+
+func FillStruct(data map[string]interface{}, result interface{}) error {
+	for k, v := range data {
+		err := SetField(result, k, v)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
