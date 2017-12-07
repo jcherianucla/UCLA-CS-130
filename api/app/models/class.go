7d4070afa074d1860420b71642d94b0edcb5eed2
jcherianucla <jcherian@ucla.edu>
2017-12-06 22:42:32 -0800

    Make models use ORM

--- api/app/models/class.go
+++ api/app/models/class.go
@@ -4,18 +4,13 @@
 package models
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/asaskevich/govalidator"
 	"github.com/jcherianucla/UCLA-CS-130/api/config/db"
 	"github.com/jcherianucla/UCLA-CS-130/api/utilities"
 	"github.com/pkg/errors"
 	"io/ioutil"
 	"net/http"
-	"reflect"
-	"strconv"
-	"strings"
 	"time"
 )
 
@@ -33,22 +28,22 @@ type ClassTable struct {
 // Validator and json tags are used for convenient serialization and
 // deserialization.
 type Class struct {
-	Id           int       `valid:"-" json:"-"`
+	Id           int64     `valid:"-" json:"-"`
 	Name         string    `valid:"required" json:"name"`
 	Description  string    `valid:"-" json:"description"`
 	Quarter      string    `valid:"required" json:"quarter"`
 	Year         string    `valid:"required" json:"year"`
-	Creator_Id   int       `valid:"-" json:"creator_id"`
+	Creator_id   int64     `valid:"-" json:"creator_id"`
 	Time_created time.Time `valid:"-" json:"-"`
 }
 
 // Represents all fields a class can be queried over.
 type ClassQuery struct {
-	Id         int
+	Id         int64
 	Name       string
 	Quarter    string
 	Year       string
-	Creator_Id int
+	Creator_id int64
 }
 
 // NewClass is used to create a new class object from an incoming HTTP request.
@@ -71,7 +66,7 @@ func (class *Class) Equals(other Class) bool {
 	return class.Name == other.Name &&
 		class.Quarter == other.Quarter &&
 		class.Year == other.Year &&
-		class.Creator_Id == other.Creator_Id
+		class.Creator_id == other.Creator_id
 }
 
 // NewClassTable creates a new table within the database for housing
@@ -85,16 +80,6 @@ func NewClassTable(db *db.Db) (classTable ClassTable, err error) {
 		return
 	}
 	classTable.connection = db
-	// Create the actual table
-	if err = classTable.createTable(); err != nil {
-		err = errors.Wrapf(err, "Could not create table on initialization")
-	}
-	return
-}
-
-// createTable runs the actual PSQL query to create the table.
-// It returns an error if one exists.
-func (table *ClassTable) createTable() (err error) {
 	query := fmt.Sprintf(`
 	CREATE TABLE IF NOT EXISTS %s(
 			id SERIAL,
@@ -105,12 +90,9 @@ func (table *ClassTable) createTable() (err error) {
 			creator_id int,
 			time_created TIMESTAMP DEFAULT now()
 		);`, CLASS_TABLE)
-
-	utilities.Sugar.Infof("SQL Query: %s", query)
-
-	// Execute the query
-	if _, err = table.connection.Pool.Exec(query); err != nil {
-		err = errors.Wrapf(err, "Table creation query failed")
+	// Create the actual table
+	if err = classTable.connection.CreateTable(query); err != nil {
+		err = errors.Wrapf(err, "Could not create table on initialization")
 	}
 	return
 }
@@ -120,75 +102,13 @@ func (table *ClassTable) createTable() (err error) {
 // It takes in the class object to insert and the user id to associate to the creator.
 // It returns the new class as in the table and an error if one exists.
 func (table *ClassTable) Insert(class Class, userId string) (new Class, err error) {
-	_, err = govalidator.ValidateStruct(class)
-	if err != nil {
-		err = errors.Wrapf(err, "Class model has invalid fields")
-		return
-	}
-	class.Creator_Id, err = strconv.Atoi(userId)
-	if err != nil {
-		err = errors.Wrapf(err, "Invalid user Id")
-		return
-	}
-	classes, err := table.Get(ClassQuery{Name: class.Name, Quarter: class.Quarter, Year: class.Year, Creator_Id: class.Creator_Id}, "AND")
-	if err != nil {
-		err = errors.Wrapf(err, "Failed to search for class")
-		return
-	} else if classes != nil {
-		err = errors.New("Class already exists")
-		return
-	}
-	var query bytes.Buffer
-	query.WriteString(fmt.Sprintf("INSERT INTO %s (", CLASS_TABLE))
-	var values []interface{}
-	var vStr, kStr bytes.Buffer
-	vIdx := 1
-	fields := reflect.ValueOf(class)
-	if fields.NumField() < 1 {
-		err = errors.New("Invalid number of fields given")
-		return
-	}
-	first := true
-	for i := 0; i < fields.NumField(); i++ {
-		k := strings.ToLower(fields.Type().Field(i).Name)
-		v := fields.Field(i).Interface()
-		// Skip auto params
-		if AUTO_PARAM[k] {
-			continue
-		}
-		if first {
-			first = false
-		} else {
-			vStr.WriteString(", ")
-			kStr.WriteString(", ")
-		}
-		kStr.WriteString(k)
-		vStr.WriteString(fmt.Sprintf("$%d", vIdx))
-		values = append(values, v)
-		vIdx++
-	}
-	query.WriteString(fmt.Sprintf("%s) VALUES (%s) RETURNING id;", kStr.String(), vStr.String()))
-
-	utilities.Sugar.Infof("SQL Query: %s", query.String())
-	utilities.Sugar.Infof("Values: %v", values)
-
-	stmt, err := table.connection.Pool.Prepare(query.String())
-	if err != nil {
-		err = errors.Wrapf(err, "Insertion query preparation failed")
-		return
-	}
-	err = stmt.QueryRow(values...).Scan(&new.Id)
-	if err != nil {
-		err = errors.Wrapf(err, "Insertion query failed to execute")
-		return
-	}
-	newID := strconv.Itoa(new.Id)
-	// Retrieve class with the new id
-	found, err := table.GetByID(newID)
+	classQuery := ClassQuery{Name: class.Name, Quarter: class.Quarter, Year: class.Year, Creator_id: class.Creator_id}
+	data, err := table.connection.Insert(CLASS_TABLE, "AND", class, classQuery)
 	if err != nil {
 		return
 	}
-	new = found
+	utilities.Sugar.Infof("Class Data: %v", data)
+	err = utilities.FillStruct(data, &new)
 	return
 }
 
@@ -198,53 +118,14 @@ func (table *ClassTable) Insert(class Class, userId string) (new Class, err erro
 // define the context of the search.
 // It returns all the found classes and an error if one exists.
 func (table *ClassTable) Get(classQuery ClassQuery, op string) (classes []Class, err error) {
-	var query bytes.Buffer
-	query.WriteString(fmt.Sprintf("SELECT * FROM %s WHERE", CLASS_TABLE))
-	// Use reflection to analyze object fields
-	fields := reflect.ValueOf(classQuery)
-	first := true
-	var values []interface{}
-	vIdx := 1
-	for i := 0; i < fields.NumField(); i++ {
-		if first {
-			query.WriteString(" ")
-			first = false
-		} else {
-			if op != "" {
-				query.WriteString(fmt.Sprintf(" %s ", op))
-			}
-		}
-		v := fields.Field(i).Interface()
-		// Skip fields that are not set to query on
-		if !utilities.IsUndeclared(v) {
-			k := strings.ToLower(fields.Type().Field(i).Name)
-			v = fmt.Sprintf("%v", v)
-			values = append(values, v)
-			query.WriteString(fmt.Sprintf("%s=$%d", k, vIdx))
-			vIdx++
-		}
-	}
-	query.WriteString(";")
-
-	utilities.Sugar.Infof("SQL Query: %s", query.String())
-	utilities.Sugar.Infof("Values: %v", values)
-
-	stmt, err := table.connection.Pool.Prepare(query.String())
+	allData, err := table.connection.Get(classQuery, op, CLASS_TABLE)
 	if err != nil {
-		err = errors.Wrapf(err, "Get query preparation failed")
 		return
 	}
-	rows, err := stmt.Query(values...)
-	if err != nil {
-		err = errors.Wrapf(err, "Get query failed to execute")
-		return
-	}
-	// Get all the class rows that matched the query
-	for rows.Next() {
-		var class Class
-		err = rows.Scan(&class.Id, &class.Name, &class.Description, &class.Quarter, &class.Year, &class.Creator_Id, &class.Time_created)
+	for _, data := range allData {
+		class := Class{}
+		err = utilities.FillStruct(data, &class)
 		if err != nil {
-			err = errors.Wrapf(err, "Row read failed")
 			return
 		}
 		classes = append(classes, class)
@@ -256,23 +137,11 @@ func (table *ClassTable) Get(classQuery ClassQuery, op string) (classes []Class,
 // It takes an ID as a string to convert to an integer to then search on.
 // It returns the found class and an error if one exists.
 func (table *ClassTable) GetByID(strId string) (class Class, err error) {
-	id, err := strconv.Atoi(strId)
+	data, err := table.connection.GetByID(strId, CLASS_TABLE)
 	if err != nil {
-		err = errors.Wrapf(err, "Invalid ID")
 		return
 	}
-	classes, err := table.Get(ClassQuery{Id: id}, "")
-	if err != nil {
-		err = errors.Wrapf(err, "Search error")
-		return
-	} else if classes == nil {
-		err = errors.New("Failed to find class")
-		return
-	} else if len(classes) != 1 {
-		err = errors.New("Found duplicate classes")
-		return
-	}
-	class = classes[0]
+	err = utilities.FillStruct(data, &class)
 	return
 }
 
@@ -280,98 +149,23 @@ func (table *ClassTable) GetByID(strId string) (class Class, err error) {
 // It takes in an id to identify the class in the DB, and updates as a class object.
 // It returns the updated class as in the DB, and an error if one exists.
 func (table *ClassTable) Update(strId string, updates Class) (updated Class, err error) {
-	class, err := table.GetByID(strId)
+	data, err := table.connection.Update(strId, CLASS_TABLE, updates)
 	if err != nil {
 		return
 	}
-	var query bytes.Buffer
-	query.WriteString(fmt.Sprintf("UPDATE %s SET", CLASS_TABLE))
-	var values []interface{}
-	vIdx := 1
-	fields := reflect.ValueOf(updates)
-	if fields.NumField() < 1 {
-		err = errors.New("Invalid number of query fields")
-		return
-	}
-	first := true
-	for i := 0; i < fields.NumField(); i++ {
-		k := strings.ToLower(fields.Type().Field(i).Name)
-		v := fields.Field(i).Interface()
-		// Skip auto params or unset fields on the incoming Class
-		if AUTO_PARAM[k] || utilities.IsUndeclared(fields.Field(i).Interface()) {
-			continue
-		}
-		if first {
-			query.WriteString(" ")
-			first = false
-		} else {
-			query.WriteString(", ")
-		}
-		values = append(values, v)
-		query.WriteString(fmt.Sprintf("%v=$%d", k, vIdx))
-		vIdx += 1
-	}
-	query.WriteString(fmt.Sprintf(" WHERE id=%s;", strId))
-
-	utilities.Sugar.Infof("SQL Query: %s", query.String())
-	utilities.Sugar.Infof("Values: %v", values)
-
-	stmt, err := table.connection.Pool.Prepare(query.String())
-	if err != nil {
-		err = errors.Wrapf(err, "Update query preparation failed")
-		return
-	}
-	if _, err = stmt.Exec(values...); err != nil {
-		err = errors.Wrapf(err, "Update query failed to execute")
-		return
-	}
-	// Get updated class
-	class, err = table.GetByID(strId)
-	if err != nil {
-		return
-	}
-	updated = class
+	err = utilities.FillStruct(data, &updated)
 	return
 }
 
 // Delete permanently removes the class object from the table.
 // It takes in an id for the class we wish to delete.
 // It returns an error if one exists.
-func (table *ClassTable) Delete(strId string) (err error) {
-	_, err = table.GetByID(strId)
-	if err != nil {
-		return
-	}
-	query := fmt.Sprintf("DELETE FROM %s WHERE id=$1;", CLASS_TABLE)
-
-	utilities.Sugar.Infof("SQL Query: %s", query)
-
-	stmt, err := table.connection.Pool.Prepare(query)
-	if err != nil {
-		err = errors.Wrapf(err, "Delete query preparation failed")
-		return
-	}
-
-	if _, err = stmt.Exec(strId); err != nil {
-		err = errors.Wrapf(err, "Delete query failed to execute")
-	}
-	return
+func (table *ClassTable) Delete(strId string) error {
+	return table.connection.Delete(strId, CLASS_TABLE)
 }
 
 // DeleteAll permanently removes all class objects from the table.
 // It returns an error if one exists.
-func (table *ClassTable) DeleteAll() (err error) {
-	query := fmt.Sprintf("DELETE FROM %s;", CLASS_TABLE)
-	utilities.Sugar.Infof("SQL Query: %s", query)
-
-	stmt, err := table.connection.Pool.Prepare(query)
-	if err != nil {
-		err = errors.Wrapf(err, "Delete all query preparation failed")
-		return
-	}
-
-	if _, err = stmt.Exec(); err != nil {
-		err = errors.Wrapf(err, "Delete all query failed to execute")
-	}
-	return
+func (table *ClassTable) DeleteAll() error {
+	return table.connection.DeleteAll(CLASS_TABLE)
 }
--- api/app/models/user.go
+++ api/app/models/user.go
@@ -1,6 +1,6 @@
 // The models package houses the Model layer within the MVC architecture design.
 // This is the lowest layer in the architecture, which directly communicates with
-// the database layer. The models represent an abstraction to the DB object relations.
+// the database layer. It utilizes the ORM for basic model functionality such as CRUD. The models represent an abstraction to the DB object relations.
 package models
 
 import (
@@ -15,24 +15,12 @@ import (
 	"golang.org/x/crypto/bcrypt"
 	"io/ioutil"
 	"net/http"
-	"reflect"
-	"strconv"
-	"strings"
 	"time"
 )
 
 // The table name as set in the Postgres DB creation.
 const (
 	USER_TABLE = "users"
-)
-
-// Params that should be automatically set by the Postgres DB,
-// that is it should never be explicitly set in any code.
-var (
-	AUTO_PARAM = map[string]bool{
-		"id":           true,
-		"time_created": true,
-	}
 )
 
 // Represents the connection to the DB instance.
@@ -44,7 +32,7 @@ type UserTable struct {
 // Validator and json tags are used for convenient serialization and
 // deserialization.
 type User struct {
-	Id int `valid:"-" json:"-"`
+	Id int64 `valid:"-" json:"-"`
 	// Distinguishes privileges between a student and professor
 	Is_professor bool      `valid:"-" json:"is_professor"`
 	Email        string    `valid:"email,required" json:"email"`
@@ -71,6 +59,7 @@ func NewUser(r *http.Request) (user User, err error) {
 	if err != nil {
 		err = errors.Wrapf(err, "Couldn't read request body")
 	}
+	utilities.Sugar.Infof("Body: %s", string(b[:]))
 	// Converts JSON to user
 	json.Unmarshal(b, &user)
 	return
@@ -85,7 +74,7 @@ func (user *User) GenerateJWT() string {
 		"exp": time.Now().Add(time.Hour * 24).Unix(),
 	})
 	utilities.Sugar.Infof("Toke: %v", token)
-	tokenString, err := token.SignedString([]byte(utilities.GP_TOKEN_SECRET))
+	tokenString, err := token.SignedString(utilities.SECRET)
 	utilities.CheckError(err)
 	utilities.Sugar.Infof("Token String: %s", tokenString)
 	return tokenString
@@ -113,16 +102,6 @@ func NewUserTable(db *db.Db) (userTable UserTable, err error) {
 		return
 	}
 	userTable.connection = db
-	// Create the actual table
-	if err = userTable.createTable(); err != nil {
-		err = errors.Wrapf(err, "Could not create table on initialization")
-	}
-	return
-}
-
-// createTable runs the actual PSQL query to create the table.
-// It returns an error if one exists.
-func (table *UserTable) createTable() (err error) {
 	query := fmt.Sprintf(`
 	CREATE TABLE IF NOT EXISTS %s(
 			id SERIAL,
@@ -133,12 +112,9 @@ func (table *UserTable) createTable() (err error) {
 			password BYTEA,
 			time_created TIMESTAMP DEFAULT now()
 		);`, USER_TABLE)
-
-	utilities.Sugar.Infof("SQL Query: %s", query)
-
-	// Execute the query
-	if _, err = table.connection.Pool.Exec(query); err != nil {
-		err = errors.Wrapf(err, "Table creation query failed")
+	// Create the actual table
+	if err = userTable.connection.CreateTable(query); err != nil {
+		err = errors.Wrapf(err, "User Table creation query failed")
 	}
 	return
 }
@@ -189,91 +165,13 @@ func (table *UserTable) Login(user User) (found User, err error) {
 // It takes in the user object to insert.
 // It returns the new user as in the table and an error if one exists.
 func (table *UserTable) Insert(user User) (new User, err error) {
-	obj, err := table.Insert(USER_TABLE, "", user, UserQuery{Email: user.Email})
+	utilities.Sugar.Infof("Password: %v", user.Password)
+	data, err := table.connection.Insert(USER_TABLE, "", user, UserQuery{Email: user.Email})
 	if err != nil {
 		return
 	}
-	utilities.FillStruct(obj, new)
+	err = utilities.FillStruct(data, &new)
 	return
-	/*
-		_, err = govalidator.ValidateStruct(user)
-		if err != nil {
-			err = errors.Wrapf(err, "User model has invalid fields")
-			return
-		}
-		users, err := table.Get(UserQuery{Email: user.Email}, "")
-		if err != nil {
-			err = errors.Wrapf(err, "Failed to search for user")
-			return
-		} else if users != nil {
-			err = errors.New("User already exists")
-			return
-		}
-		var query bytes.Buffer
-		query.WriteString(fmt.Sprintf("INSERT INTO %s (", USER_TABLE))
-		var values []interface{}
-		var vStr, kStr bytes.Buffer
-		vIdx := 1
-		fields := reflect.ValueOf(user)
-		if fields.NumField() < 1 {
-			err = errors.New("Invalid number of fields given")
-			return
-		}
-		first := true
-		for i := 0; i < fields.NumField(); i++ {
-			k := strings.ToLower(fields.Type().Field(i).Name)
-			v := fields.Field(i).Interface()
-			// Skip auto params
-			if AUTO_PARAM[k] {
-				continue
-			}
-			if first {
-				first = false
-			} else {
-				vStr.WriteString(", ")
-				kStr.WriteString(", ")
-			}
-			kStr.WriteString(k)
-			vStr.WriteString(fmt.Sprintf("$%d", vIdx))
-			// Hash the password
-			if k == "Password" {
-				vStr, _ := v.(string)
-				var hash []byte
-				hash, err = bcrypt.GenerateFromPassword([]byte(vStr), bcrypt.DefaultCost)
-				if err != nil {
-					err = errors.Wrapf(err, "Password hash failed")
-					return
-				}
-				values = append(values, hash)
-			} else {
-				values = append(values, v)
-			}
-			vIdx++
-		}
-		query.WriteString(fmt.Sprintf("%s) VALUES (%s) RETURNING id;", kStr.String(), vStr.String()))
-
-		utilities.Sugar.Infof("SQL Query: %s", query.String())
-		utilities.Sugar.Infof("Values: %v", values)
-
-		stmt, err := table.connection.Pool.Prepare(query.String())
-		if err != nil {
-			err = errors.Wrapf(err, "Insertion query preparation failed")
-			return
-		}
-		err = stmt.QueryRow(values...).Scan(&new.Id)
-		if err != nil {
-			err = errors.Wrapf(err, "Insertion query failed to execute")
-			return
-		}
-		newID := strconv.Itoa(new.Id)
-		// Retrieve user with the new id
-		found, err := table.GetByID(newID)
-		if err != nil {
-			return
-		}
-		new = found
-		return
-	*/
 }
 
 // Get attempts to provide a generalized search through the user table based on the
@@ -282,53 +180,14 @@ func (table *UserTable) Insert(user User) (new User, err error) {
 // define the context of the search.
 // It returns all the found users and an error if one exists.
 func (table *UserTable) Get(userQuery UserQuery, op string) (users []User, err error) {
-	var query bytes.Buffer
-	query.WriteString(fmt.Sprintf("SELECT * FROM %s WHERE", USER_TABLE))
-	// Use reflection to analyze object fields
-	fields := reflect.ValueOf(userQuery)
-	first := true
-	var values []interface{}
-	vIdx := 1
-	for i := 0; i < fields.NumField(); i++ {
-		if first {
-			query.WriteString(" ")
-			first = false
-		} else {
-			if op != "" {
-				query.WriteString(fmt.Sprintf(" %s ", op))
-			}
-		}
-		v := fields.Field(i).Interface()
-		// Skip fields that are not set to query on
-		if !utilities.IsUndeclared(v) {
-			k := strings.ToLower(fields.Type().Field(i).Name)
-			v = fmt.Sprintf("%v", v)
-			values = append(values, v)
-			query.WriteString(fmt.Sprintf("%s=$%d", k, vIdx))
-			vIdx++
-		}
-	}
-	query.WriteString(";")
-
-	utilities.Sugar.Infof("SQL Query: %s", query.String())
-	utilities.Sugar.Infof("Values: %v", values)
-
-	stmt, err := table.connection.Pool.Prepare(query.String())
+	allData, err := table.connection.Get(userQuery, op, USER_TABLE)
 	if err != nil {
-		err = errors.Wrapf(err, "Get query preparation failed")
 		return
 	}
-	rows, err := stmt.Query(values...)
-	if err != nil {
-		err = errors.Wrapf(err, "Get query failed to execute")
-		return
-	}
-	// Get all the user rows that matched the query
-	for rows.Next() {
-		var user User
-		err = rows.Scan(&user.Id, &user.Is_professor, &user.Email, &user.First_name, &user.Last_name, &user.Password, &user.Time_created)
+	for _, data := range allData {
+		user := User{}
+		err = utilities.FillStruct(data, &user)
 		if err != nil {
-			err = errors.Wrapf(err, "Row read failed")
 			return
 		}
 		users = append(users, user)
@@ -340,23 +199,11 @@ func (table *UserTable) Get(userQuery UserQuery, op string) (users []User, err e
 // It takes an ID as a string to convert to an integer to then search on.
 // It returns the found user and an error if one exists.
 func (table *UserTable) GetByID(strId string) (user User, err error) {
-	id, err := strconv.Atoi(strId)
+	data, err := table.connection.GetByID(strId, USER_TABLE)
 	if err != nil {
-		err = errors.Wrapf(err, "Invalid ID")
 		return
 	}
-	users, err := table.Get(UserQuery{Id: id}, "")
-	if err != nil {
-		err = errors.Wrapf(err, "Search error")
-		return
-	} else if users == nil {
-		err = errors.New("Failed to find user")
-		return
-	} else if len(users) != 1 {
-		err = errors.New("Found duplicate users")
-		return
-	}
-	user = users[0]
+	err = utilities.FillStruct(data, &user)
 	return
 }
 
@@ -364,109 +211,23 @@ func (table *UserTable) GetByID(strId string) (user User, err error) {
 // It takes in an id to identify the user in the DB, and updates as a user object.
 // It returns the updated user as in the DB, and an error if one exists.
 func (table *UserTable) Update(strId string, updates User) (updated User, err error) {
-	user, err := table.GetByID(strId)
+	data, err := table.connection.Update(strId, USER_TABLE, updates)
 	if err != nil {
 		return
 	}
-	var query bytes.Buffer
-	query.WriteString(fmt.Sprintf("UPDATE %s SET", USER_TABLE))
-	var values []interface{}
-	vIdx := 1
-	fields := reflect.ValueOf(updates)
-	if fields.NumField() < 1 {
-		err = errors.New("Invalid number of query fields")
-		return
-	}
-	first := true
-	for i := 0; i < fields.NumField(); i++ {
-		k := strings.ToLower(fields.Type().Field(i).Name)
-		v := fields.Field(i).Interface()
-		// Skip auto params or unset fields on the incoming User
-		if AUTO_PARAM[k] || utilities.IsUndeclared(fields.Field(i).Interface()) {
-			continue
-		}
-		if first {
-			query.WriteString(" ")
-			first = false
-		} else {
-			query.WriteString(", ")
-		}
-		// Hash new password
-		if k == "Password" {
-			vStr := v.(string)
-			hash, cryptErr := bcrypt.GenerateFromPassword([]byte(vStr), bcrypt.DefaultCost)
-			if cryptErr != nil {
-				err = errors.Wrapf(cryptErr, "Password hash failed")
-				return
-			}
-			values = append(values, hash)
-		} else {
-			values = append(values, v)
-		}
-		query.WriteString(fmt.Sprintf("%v=$%d", k, vIdx))
-		vIdx += 1
-	}
-	query.WriteString(fmt.Sprintf(" WHERE id=%s;", strId))
-
-	utilities.Sugar.Infof("SQL Query: %s", query.String())
-	utilities.Sugar.Infof("Values: %v", values)
-
-	stmt, err := table.connection.Pool.Prepare(query.String())
-	if err != nil {
-		err = errors.Wrapf(err, "Update query preparation failed")
-		return
-	}
-	if _, err = stmt.Exec(values...); err != nil {
-		err = errors.Wrapf(err, "Update query failed to execute")
-		return
-	}
-	// Get updated user
-	user, err = table.GetByID(strId)
-	if err != nil {
-		return
-	}
-	updated = user
+	err = utilities.FillStruct(data, &updated)
 	return
 }
 
 // Delete permanently removes the user object from the table.
 // It takes in an id for the user we wish to delete.
 // It returns an error if one exists.
-func (table *UserTable) Delete(strId string) (err error) {
-	_, err = table.GetByID(strId)
-	if err != nil {
-		return
-	}
-	query := fmt.Sprintf("DELETE FROM %s WHERE id=$1;", USER_TABLE)
-
-	utilities.Sugar.Infof("SQL Query: %s", query)
-
-	stmt, err := table.connection.Pool.Prepare(query)
-	if err != nil {
-		err = errors.Wrapf(err, "Delete query preparation failed")
-		return
-	}
-
-	if _, err = stmt.Exec(strId); err != nil {
-		err = errors.Wrapf(err, "Delete query failed to execute")
-	}
-	return
+func (table *UserTable) Delete(strId string) error {
+	return table.connection.Delete(strId, USER_TABLE)
 }
 
 // DeleteAll permanently removes all user objects from the table.
 // It returns an error if one exists.
-func (table *UserTable) DeleteAll() (err error) {
-	query := fmt.Sprintf("DELETE FROM %s;", USER_TABLE)
-	utilities.Sugar.Infof("SQL Query: %s", query)
-
-	stmt, err := table.connection.Pool.Prepare(query)
-	if err != nil {
-		err = errors.Wrapf(err, "Delete all query preparation failed")
-		return
-	}
-
-	if _, err = stmt.Exec(); err != nil {
-		err = errors.Wrapf(err, "Delete all query failed to execute")
-	}
-	return
+func (table *UserTable) DeleteAll() error {
+	return table.connection.DeleteAll(USER_TABLE)
 }
