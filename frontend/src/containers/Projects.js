111ad661a5fa897b39cc6b2f60218a2324573061
Shalini Dangi <shalinidangi@ucla.edu>
2017-12-10 14:12:35 -0800

    Fix margins on Projects pages

--- frontend/src/containers/Projects.js
+++ frontend/src/containers/Projects.js
@@ -67,7 +67,6 @@ class Projects extends Component {
         <SidePanel />
         <div className="page">
           <Header title="Welcome!" path={["Classes", ["Projects", this.props.match.params.class_id]]}/>
-          <br /> <br />
           <p ref="error" className="red"></p>
           <Grid fluid>
               <Row>
