c4dcba636e7cbe289a7e62663a03db53224d27e1
jcherianucla <jcherian@ucla.edu>
2017-12-11 02:17:29 -0800

    Add execution engine for grading

--- api/app/models/submission.go
+++ api/app/models/submission.go
@@ -6,7 +6,11 @@ import (
 	"github.com/jcherianucla/UCLA-CS-130/api/config/db"
 	"github.com/jcherianucla/UCLA-CS-130/api/utilities"
 	"github.com/pkg/errors"
+	"io/ioutil"
 	"net/http"
+	"os/exec"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -26,13 +30,13 @@ type SubmissionTable struct {
 type Submission struct {
 	Id            int64     `valid:"-" json:"-"`
 	File          []byte    `valid:"required" json:"file"`
-	Score         int       `valid:"-" json:"score"`
+	Score         int64     `valid:"-" json:"score"`
 	Pre_results   []string  `valid:"-" json:"pre_results"`
 	Post_results  []string  `valid:"-" json:"post_results"`
 	Assignment_id int64     `valid:"required" json:"assignment_id"`
 	User_id       int64     `valid:"required" json:"student_id"`
 	Time_created  time.Time `valid:"-" json:"-"`
-	Time_updated  time.Time `valid:"-" json:"-"`
+	Time_updated  time.Time `valid:"-" json:"updated_at"`
 }
 
 // Represents all fields a submission can be queried over.
@@ -42,6 +46,31 @@ type SubmissionQuery struct {
 	User_id       int64
 }
 
+func Exec(grade_script, s_file []byte, lang utilities.Language) (score int64, results []string, err error) {
+	err = ioutil.WriteFile("script.sh", grade_script, 0666)
+	if err != nil {
+		err = errors.Wrapf(err, "Failed to create script file")
+		return
+	}
+	s_filepath := "submitted." + strings.ToLower(utilities.GetLanguage(lang))
+	err = ioutil.WriteFile(s_filepath, s_file, 0777)
+	if err != nil {
+		err = errors.Wrapf(err, "Failed to create submitted file")
+		return
+	}
+	out, err := exec.Command("bash", "script.sh", s_filepath).CombinedOutput()
+	if err != nil {
+		err = errors.Wrapf(err, "Couldn't execute script on submitted file")
+		return
+	}
+	utilities.Sugar.Infof("Output: %v", out)
+	outStr := string(out)
+	res := strings.Split(outStr, "\n")
+	score, _ = strconv.ParseInt(res[0][len("Score: "):], 10, 64)
+	results = res[1:]
+	return
+
+}
 func NewSubmission(r *http.Request) (submission Submission, err error) {
 	err = r.ParseMultipartForm(utilities.MAX_STORAGE)
 	if err != nil {
@@ -69,8 +98,6 @@ func NewSubmissionTable(db *db.Db) (submissionTable SubmissionTable, err error) 
 			id SERIAL,
 			file BYTEA,
 			score INT,
-			pre_results TEXT[],
-			post_results TEXT[],
 			assignment_id INT,
 			user_id INT,
 			time_created TIMESTAMP DEFAULT now(),
@@ -90,6 +117,7 @@ func (table *SubmissionTable) Insert(submission Submission) (new Submission, err
 		return
 	}
 	err = utilities.FillStruct(data, &new)
+	utilities.Sugar.Infof("Insert submission: %v", new)
 	return
 }
 
@@ -109,12 +137,18 @@ func (table *SubmissionTable) Get(submissionQuery SubmissionQuery, op string) (s
 	return
 }
 
-func (table *SubmissionTable) GetByID(strId string) (submission Submission, err error) {
-	data, err := table.connection.GetByID(strId, SUBMISSION_TABLE)
+func (table *SubmissionTable) GetByID(uid, aid string) (submission Submission, err error) {
+	_uid, _ := strconv.ParseInt(uid, 10, 64)
+	_aid, _ := strconv.ParseInt(aid, 10, 64)
+	objs, err := table.Get(SubmissionQuery{User_id: _uid, Assignment_id: _aid}, "AND")
 	if err != nil {
 		return
 	}
-	err = utilities.FillStruct(data, &submission)
+	if len(objs) == 0 {
+		err = errors.New("Couldn't find object")
+	} else {
+		submission = objs[0]
+	}
 	return
 }
 
